d42c2e0121582cc5e4e2acad4a4d7a7b48149015
abaranov <andrei91077@gmail.com>
2022-06-13 15:54:24 +0200

    Added first Example with join() method

---
+++ app/src/main/java/com/andrei/courutinesexample/FirstActivity.kt
@@ -0,0 +1,65 @@
+package com.andrei.courutinesexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.andrei.courutinesexample.databinding.ActivityFirstBinding
+import com.andrei.courutinesexample.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/** На этом экране пример использования join()
+ *  Имеется три корутины.
+ *  В первой имитация загрузки города, которая происходит с некторой задержкой.
+ *  Во второй то же самое, только для температуры.
+ */
+
+class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLoad.setOnClickListener {
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+
+            /** Первая корутина */
+            val jobCity = lifecycleScope.launch {
+                val city = loadCity()
+                binding.tvLocation.text = city
+            }
+            /** Вторая корутина */
+            val jobTemp = lifecycleScope.launch {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
+            /** Третья корутина
+             * В ней мы используем метод join() для первых двух корутин.
+             * Благодаря ему корутина ожидает завершения работы 1 и 2 корутины.
+             * И только после их завершения идет дальше.
+             */
+            lifecycleScope.launch {
+                jobCity.join()
+                jobTemp.join()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+        private suspend fun loadCity(): String {
+            delay(500)
+            return "Moscow"
+        }
+
+        private suspend fun loadTemperature(): Int {
+            delay(3000)
+            return 17
+        }
+
+}
--- app/src/main/java/com/andrei/courutinesexample/MainActivity.kt
+++ app/src/main/java/com/andrei/courutinesexample/MainActivity.kt
@@ -1,63 +1,35 @@
  package com.andrei.courutinesexample
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.andrei.courutinesexample.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
  class MainActivity : AppCompatActivity() {
 
-    private var list: ArrayList<Int> = arrayListOf()
+     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d("TAG", "OnCrate в ${Thread.currentThread().name}")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.button).setOnClickListener {
-            Toast.makeText(this, "Тра-та-та", Toast.LENGTH_SHORT).show()
-        }
-        findViewById<Button>(R.id.buttonStart).setOnClickListener {
-//            test()
-//            coroutineTest()
-        GlobalScope.launch (Dispatchers.IO){
-            coroutineTest()
-//            withContext(Dispatchers.Main){
-//                Toast.makeText(this@MainActivity, "Тра-та-та", Toast.LENGTH_SHORT).show()
-//                coroutineTest2()
-//            }
-        }
-            GlobalScope.launch (Dispatchers.IO){
-                coroutineTest()
+        binding.apply {
+            btn1.setOnClickListener {
+                val intent = Intent(this@MainActivity, FirstActivity::class.java)
+                startActivity(intent)
+            }
+            btn2.setOnClickListener {
+
+            }
+            btn3.setOnClickListener {
+
             }
         }
-
     }
-
-   suspend fun coroutineTest(){
-        Log.d("TAG", "Метод coroutineTest начат ${Thread.currentThread().name} потоке")
-       delay(2500)
-        Log.d("TAG", "Метод coroutineTest идет")
-       delay(2500)
-        Log.d("TAG", "Метод coroutineTest закончен")
-    }
-
-     suspend fun coroutineTest2(){
-         Log.d("TAG", "Метод coroutineTest2 начат ${Thread.currentThread().name} потоке")
-         delay(2500)
-         Log.d("TAG", "Метод coroutineTest2 идет")
-         delay(2500)
-         Log.d("TAG", "Метод coroutineTest2 закончен")
-     }
-
-     fun test (){
-
-         Log.d("TAG", "Метод test начат ${Thread.currentThread().name} потоке")
-         Thread.sleep(2500)
-         Log.d("TAG", "Метод test идет")
-         Thread.sleep(2500)
-         Log.d("TAG", "Метод test закончен")
-     }
 }
